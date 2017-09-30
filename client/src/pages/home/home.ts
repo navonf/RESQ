fc3160d9826b1ae50066cd5883a5ab4113cbd266
Jose Pena <josepena@MacBook-Pro-de-Jose.local>
2017-09-30 02:27:37 -0400

    added map

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
+  selector: 'home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor(public navCtrl: NavController) {
 
   }
+
+  title: string = "Test ShellHacks2017";
+  lat: number = 25.759591;
+  lng: number = -80.374240;
+
+  ucflat = 28.6024274;
+  ucflng = -81.2000599;
+  // icon = "https://www.shareicon.net/data/128x128/2016/04/21/753224_hospital_512x512.png";
+  icon = "assets/icon.png";
+  @ViewChild('window')
+  w;
+
+
+  ngOnInit() {
+
+  }
+
+  open() {
+  	console.log("test");
+  	this.w.open();
+  }
+
 
 }
